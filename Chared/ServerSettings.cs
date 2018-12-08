5057a9de39def89b1e3ff9822a93e161884b04c0
John Larsen <john@pcis.se>
2018-12-08 19:29:51 +0100

    Some comments

--- Chared/ServerSettings.cs
+++ Chared/ServerSettings.cs
@@ -2,7 +2,11 @@
 {
 	public class ServerSettings
 	{
-		public string CheckUpdateUrl { get; set; }
+		public string CheckUpdateUrl { get; set; } // url response with latest version 
 		public string VersionBaseUrl { get; set; }
+
+		//URL to content file will be versionBAseRl+VErsionName +.json 
+
+
 	}
 }
